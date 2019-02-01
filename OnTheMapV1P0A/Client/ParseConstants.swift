248ee86dadd57db55804870fa6669080c6121431
FarhanaQazi <farhan.abu.qazi@att.net>
2019-01-31 21:26:20 -0500

    minor changes

--- OnTheMapV1P0A/Client/ParseConstants.swift
+++ OnTheMapV1P0A/Client/ParseConstants.swift
@@ -8,13 +8,13 @@
 import Foundation
 extension ParseClient {
     
-    //MARK : -  Constants
+
     struct Constants {
-        //MARK API Key 
+
         static let ParseApplicationID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
         static let ParseAPIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
         
-        //MARK: - Parse URL
+
         static let ParseScheme = "https"
         static let ParseHost = "parse.udacity.com"
         static let ParsePath = "/parse/classes"
